ff9cfb2071402d8836441094668de38004c6ccb2
wa3lily <liik@mail.ru>
2020-09-17 16:26:36 +0300

    exceptions

---
+++ src/main/java/exceptions_hw/TerminalImpl.java
@@ -0,0 +1,29 @@
+package exceptions_hw;
+
+import java.util.Scanner;
+
+public class TerminalImpl implements Terminal{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        String pin="";
+        pin=sc.next();
+
+
+
+    }
+
+    @java.lang.Override
+    public void checkAcct() {
+
+    }
+
+    @java.lang.Override
+    public void takeOffMoney() {
+
+    }
+
+    @java.lang.Override
+    public void putMoney() {
+
+    }
+}
