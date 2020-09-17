d0b1c7527112d78dcadaf3f1b38b99424a916be1
lily <liik@mail.ru>
2020-09-17 13:30:15 +0300

    generics2

---
+++ src/main/java/generics_hw/CollectionUtils.java
@@ -0,0 +1,39 @@
+package generics_hw;
+
+import com.sun.tools.javac.util.List;
+
+import java.util.Comparator;
+
+public class CollectionUtils {
+    public static<T> void addAll(List<? extends T> source, List<? super T> destination) {
+        destination.addAll(source);
+    }
+    public static List newArrayList() {
+        return null;
+    }
+    public static int indexOf(List source, Object o) {
+        return 0;
+    }
+    public static List limit(List source, int size) {
+        return null;
+    }
+    public static void add(List source, Object o) {
+
+    }
+    public static void removeAll(List removeFrom, List c2) {
+
+    }
+    public static boolean containsAll(List c1, List c2) {
+        return false;
+
+    }
+    public static boolean containsAny(List c1, List c2) {
+        return false;
+    }
+    public static List range(List list, Object min, Object max) {
+        return null;
+    }
+    public static List range(List list, Object min, Object max, Comparator comparator) {
+        return null;
+    }
+}
